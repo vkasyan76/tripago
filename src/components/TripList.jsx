ceb9738e23d66dcd22ae516b9140331b43ba2c28
vkasyan76 <vkasyan@gmx.de>
2022-12-31 13:31:55 +0100

    UseEffect Dependency Array Fetch Buttons

--- src/components/TripList.jsx
+++ src/components/TripList.jsx
@@ -4,12 +4,13 @@ import './TripList.css'
 
 export default function TripList() {
   const [trips, setTrips] = useState([])
+  const [url, setUrl] = useState('http://localhost:3000/trips')
 
   useEffect(() => {
-    fetch('http://localhost:3000/trips')
+    fetch(url)
       .then((response) => response.json())
       .then((json) => setTrips(json))
-  }, [])
+  }, [url])
 
   console.log(trips)
 
@@ -24,6 +25,16 @@ export default function TripList() {
           </li>
         ))}
       </ul>
+      <div className="filters">
+        <button
+          onClick={() => setUrl('http://localhost:3000/trips?loc=europe')}
+        >
+          European Trips
+        </button>
+        <button onClick={() => setUrl('http://localhost:3000/trips')}>
+          All Trips
+        </button>
+      </div>
     </div>
   )
 }

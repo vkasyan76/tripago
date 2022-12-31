a2da873468e54c6f3f40ee93ce96590d528174fa
vkasyan76 <vkasyan@gmx.de>
2022-12-31 12:39:52 +0100

    Fetching Data & UseEffect

---
+++ src/components/TripList.jsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react'
+// styles
+import './TripList.css'
+
+export default function TripList() {
+  const [trips, setTrips] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:3000/trips')
+      .then((response) => response.json())
+      .then((json) => setTrips(json))
+  }, [])
+
+  console.log(trips)
+
+  return (
+    <div className="trip-list">
+      <h2>Trip List</h2>
+      <ul>
+        {trips.map((trip) => (
+          <li key={trip.id}>
+            <h3>{trip.title}</h3>
+            <p>{trip.price}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}

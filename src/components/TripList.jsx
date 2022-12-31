3420b1144c38b4e76d2eeb66c36fc62f9313f5b8
vkasyan76 <vkasyan@gmx.de>
2022-12-31 19:32:00 +0100

    Use Callback for Function Dependencies

--- src/components/TripList.jsx
+++ src/components/TripList.jsx
@@ -1,4 +1,5 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useCallback } from 'react'
+
 // styles
 import './TripList.css'
 
@@ -6,11 +7,21 @@ export default function TripList() {
   const [trips, setTrips] = useState([])
   const [url, setUrl] = useState('http://localhost:3000/trips')
 
-  useEffect(() => {
-    fetch(url)
-      .then((response) => response.json())
-      .then((json) => setTrips(json))
+  const fetchTrips = useCallback(async () => {
+    const response = await fetch(url)
+    const json = await response.json()
+    setTrips(json)
   }, [url])
+
+  useEffect(() => {
+    // fetch(url)
+    //   .then((response) => response.json())
+    //   .then((json) => setTrips(json))
+    fetchTrips()
+  }, [
+    // url,
+    fetchTrips,
+  ])
 
   console.log(trips)
 
